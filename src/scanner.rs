cafe5b742f8051e5cf7cb64c2e1dbba54ee35608
Will Turner <rwturner17@gmail.com>
2017-08-26 15:33:18 -0400

    Filled in tokens for Lox

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,9 +1,48 @@
+use std::fmt;
+
 #[derive(Debug)]
-pub enum Token {
-	TokenA,
-	TokenB,
+#[allow(dead_code)]
+pub enum TokenType {
+	// Single-character tokens.
+	LeftParen, RightParen, LeftBrace, RightBrace,
+	Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+
+	// One or two character tokens.
+	Bang, BangEqual,
+	Equal, EqualEqual,
+	Greater, GreaterEqual,
+	Less, LessEqual,
+
+	// Literals.
+	Identifier, StringLiteral, Number,
+
+	// Keywords.
+	And, Class, Else, False, Fun, For, If, Nil, Or,
+	Print, Return, Super, This, True, Var, While,
+
+	Eof,
+}
+
+#[derive(Debug)]
+pub struct Token {
+  token_type: TokenType,
+  lexeme: String,
+  //Object literal;
+  line: u32,
+}
+impl Token {
+	pub fn new(token_type: TokenType, lexeme: String, line: u32) -> Self {
+		Token{ token_type: token_type, lexeme: lexeme, line: line}
+	}
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({:?}, {})", self.token_type, self.lexeme)
+    }
 }
 
 pub fn scan_tokens(text: &str) -> Vec<Token> {
-	vec![Token::TokenA, Token::TokenB]
+	vec![Token::new(TokenType::LeftParen, "(".to_string(), 0),
+	     Token::new(TokenType::RightParen, ")".to_string(), 1)]
 }
